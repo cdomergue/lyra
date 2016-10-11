74ecf61a5e677c45a4ca758ee1bac38d38b9e6f2
Christophe Domergue <christophe.domergue@zenika.com>
2016-10-11 15:59:18 +0200

    Ajout d'une activité Maps et lien avec le login

--- app/src/main/java/bastide/domergue/lyra/LoginActivity.java
+++ app/src/main/java/bastide/domergue/lyra/LoginActivity.java
@@ -3,6 +3,7 @@ package bastide.domergue.lyra;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -334,6 +336,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+                Log.i("Connection", "Connection réussie");
+                Intent nextActivity = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(nextActivity);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
